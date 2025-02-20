1aa83ec374efc76c280ab47794f878462155f5e1
Emilie <emmi0002@stud.kea.dk>
2025-02-20 11:31:45 +0100

    filter done

--- js/Recipes.js
+++ js/Recipes.js
@@ -1,27 +1,48 @@
 const listContainer = document.querySelector(".recipe_list");
+const countryFilter = document.querySelector("#countryFilter");
 
 // const myCategory = new URLSearchParams(window.location.search).get("category");
+function showRecipes(event) {
+  fetch(`https://dummyjson.com/recipes?limit=100`)
+    .then((response) => response.json())
+    .then((recipes) => {
+      let markup = recipes.recipes
+        .filter((recipe) => {
+          if (event) {
+            if (event.target.value == "Italian") {
+              return recipe.cuisine == "Italian";
+            }
+            if (event.target.value == "Mexican") {
+              return recipe.cuisine == "Mexican";
+            }
+            if (event.target.value == "Indian") {
+              return recipe.cuisine == "Indian";
+            }
+            if (event.target.value == "Asian") {
+              return recipe.cuisine == "Asian";
+            }
+            else {
+              return true;
+            }
+          } else {
+            return true;
+          }
+        })
+        .map((recipe) => {
+          return `
+          <div class="grid_1-1">
+              <img src="https://cdn.dummyjson.com/recipe-images/${recipe.id}.webp" alt="Dish">
+              <p>${recipe.name}</p>
+              <a href="dish.html?id=${recipe.id}" class="button">Go to recepie</a>
+          </div>`;
+        })
+        .join("");
 
-fetch(`https://dummyjson.com/recipes?limit=100`)
-  .then((response) => response.json())
-  .then(showList);
-
-function showList(recipes) {
-  console.log(recipes);
-
-  let markup = "";
-
-  recipes.recipes
-    .map((recipe) => {
-      markup += `
-        <div class="grid_1-1">
-            <img src="https://cdn.dummyjson.com/recipe-images/${recipe.id}.webp" alt="Dish">
-            <p>${recipe.name}</p>
-            <a href="dish.html?id=${recipe.id}" class="button">Go to recepie</a>
-        </div>`;
-    })
-    .join("");
-
-  console.log(markup);
-  listContainer.innerHTML = markup;
+      console.log(markup);
+      listContainer.innerHTML = markup;
+    });
 }
+
+countryFilter.addEventListener("change", showRecipes);
+
+showRecipes();
